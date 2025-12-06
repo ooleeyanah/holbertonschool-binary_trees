331c3763bb2ce2e20c72e7ef54c00bade32ee346
Uliana Deshin <uliana.deshin@gmail.com>
2025-12-06 03:43:57 +0000

    not finished yet, just checking if it saves to correct dir

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,10 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_insert_left - inserts left node
+ * @parent: parent node
+ * @value: value of node
+ * Return: pointer to new node or NULL
+ */
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+  
