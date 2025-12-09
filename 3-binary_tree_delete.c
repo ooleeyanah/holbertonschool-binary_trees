c3deb9350b3bbd1c61137ba06bab928cc2ad433e
Uliana Deshin <uliana.deshin@gmail.com>
2025-12-09 03:28:14 +0000

    delete binary tree

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,14 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_delete - deletes binary tree
+ * @tree: pointer to root node to delete
+ * Return: no return
+ */
+void binary_tree_delete(binary_tree_t *tree)
+{
+if (tree == NULL)
+return;
+binary_tree_delete(tree->left);
+binary_tree_delete(tree->right);
+free(tree);
+}
