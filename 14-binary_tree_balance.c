e425c1aefefe54194c9cb354fa383933163f848c
Uliana Deshin <uliana.deshin@gmail.com>
2025-12-10 00:53:09 +0000

    added height function

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,10 +1,10 @@
 #include "binary_trees.h"
 /**
- * binary_tree_balance - measure balance of bt
+ * binary_tree_height - measure height of bt
  * @tree: pointer to root node
  * Return: measure of balance or 0 if NULL
  */
-int binary_tree_balance(const binary_tree_t *tree)
+int binary_tree_height(const binary_tree_t *tree)
 {
 size_t left_h = 0, right_h = 0;
 if (tree == NULL)
@@ -14,4 +14,18 @@ left_h = 1 + binary_tree_height(tree->left);
 if (tree->right)
 right_h = 1 + binary_tree_height(tree->right);
 return (left_h > right_h ? left_h : right_h);
+}
+/**
+ * binary_tree_balance - measures balance of bt
+ * @tree: pointer to root node
+ * Return: balance or 0 if NULL
+ */
+int binary_tree_balance(const binary_tree_t *tree)
+{
+int left_h, right_h;
+if (tree == NULL)
+return (0);
+left_h = (tree->left ? (int)binary_tree_height(tree->left) + 1 : 0);
+right_h = (tree->right ? (int)binary_tree_height(tree->right) + 1 : 0);
+return (left_h - right_h);
 }
