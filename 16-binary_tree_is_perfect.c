cd5a25f0cfd7c696fa384953425e0641d25c851c
Uliana Deshin <uliana.deshin@gmail.com>
2025-12-11 02:44:45 +0000

    added size and height funcs

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,5 +1,33 @@
 #include "binary_trees.h"
 /**
+ * binary_tree_height - measures bt height
+ * @tree: pointer to root node
+ * Return: height or 0
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+size_t left_h, right_h;
+if (tree == NULL)
+return (0);
+if (tree->left == NULL && tree->right == NULL)
+return (0);
+left_h = binary_tree_height(tree->left);
+right_h = binary_tree_height(tree->right);
+return ((left_h > right_h ? left_h : right_h) +1);
+}
+/**
+ * binary_tree_size - measures size of bianry tree
+ * @tree: pointer to root node
+ * Return: size or NULL
+ */
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+if (tree == NULL)
+return (0);
+return (1 + binary_tree_size(tree->left)
++ binary_tree_size(tree->right));
+}
+/**
  * binary_tree_is_perfect - checks if bt is perfect
  * @tree: pointer to root node
  * Return: 1 or otherwise 0 if NULL
